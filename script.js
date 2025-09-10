2cc8acc7e89c4ad01b7bf088d90355546fa1463b
Ibuprofen-lab <jaynepawlikowski24@hotmail.com>
2025-09-11 00:21:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let numberToGuess = Math.round(Math.random() * 100);
+let tries = 0;
+
+function guessTheNumber() {
+    tries = tries + 1;
+    displayTries.innerHTML = 'Versuche: ' + tries
+    
+    if(numberToGuess == myNumber.value) {
+        headline.innerHTML = 'Du hast gewonnen! 🤞🤞💕';
+    }
+
+    if(numberToGuess > myNumber.value) {
+        headline.innerHTML = 'Die Zahl ist größer!';
+    }
+
+    if(numberToGuess < myNumber.value) {
+        headline.innerHTML = 'Die Zahl ist kleiner!';
+    }
+}
